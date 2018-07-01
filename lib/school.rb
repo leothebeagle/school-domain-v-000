3905897efe4d526205c21a77ed688544bc0179ab
Aziz Kosber <a.kosber@gmail.com>
2018-07-01 18:23:30 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,14 +7,7 @@ class School
     @roster = {}
   end
 
-  def add_student(name, grade)
-    if roster.include?(grade)
-      roster[grade] << name
-    else
-      roster[grade] = []
-      roster[grade] << name
-    end
-  end
+  
 
   def add_student(name, grade)
     if roster[grade] |= roster[grade] = []
